1b7435fdbd31f5dd9e90f647edcbcd16ff4150cd
JoelNickson <joel2000nickson@gmail.com>
2022-02-27 23:47:03 +0530

    build with react redux docs

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
 
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+import TodoApp from "./TodoApp";
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
+  rootElement
+);
